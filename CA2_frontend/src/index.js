765475b143d67373f22d2b8f74636c54ed693777
Frederik Løwe Lassen <frizz97@hotmail.com>
2019-10-05 19:02:20 +0200

    more changes with mroe methods to apidoc

--- CA2_frontend/src/index.js
+++ CA2_frontend/src/index.js
@@ -24,7 +24,39 @@ document.getElementById("but1").addEventListener("click", function(){
             "<th>user (with the requested id)</th>"+
             "<th>(e1)</th>" +
         "</tr>" +
-    "</table>";
+        "<tr>" +
+            "<th>POST</th>" +
+            "<th>api/person/</th>"+
+            "<th>User 1 no ID</th>"+
+            "<th></th>"+
+            "<th></th>" +
+        "</tr>" +
+        "<tr>" +
+            "<th>PUT</th>" +
+            "<th>api/person/{id}</th>"+
+            "<th>User 1 ID</th>"+
+            "<th>Requested user with changes</th>"+
+            "<th></th>" +
+        "</tr>" +
+        "<tr>" +
+            "<th>DELETE</th>" +
+            "<th>api/person/{id}</th>"+
+            "<th>User 1 ID</th>"+
+            "<th>(maybe an OK message)</th>"+
+            "<th></th>" +
+        "</tr>" +
+    "</table>" +
+    "<em>" +
+    "Request Body and Response Formats<br>" +
+    "(1) User format (don’t provide ID, for POST<br>" +
+"{<br>"+
+    "&emsp;&emsp; " + "id" + ": Number,<br>" +
+    "&emsp;&emsp; " + "firstname" + ": String,<br>" +
+    "&emsp;&emsp; " + "lastname" + ": String,<br>" +
+    "&emsp;&emsp; " + "phone" +": Number,<br>" +
+    "&emsp;&emsp; " + "email" + ": String (email)<br>" +
+"}<br>" +
+"</em>" ;
 });
 
 
