1f6828fcfd3e5417f132a41dcbe9357ab5cd39af
Frederik Løwe Lassen <frizz97@hotmail.com>
2019-10-05 15:34:33 +0200

    ændret apidoc til spa men stadig meh løsning

--- CA2_frontend/src/index.js
+++ CA2_frontend/src/index.js
@@ -1,8 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import jokes from "./jokes";
 
-const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
-document.getElementById("jokes").innerHTML = allJokes.join("");
+document.getElementById("but1").addEventListener("click", function(){
+    document.getElementById("div1").innerHTML = "<table border=" + "1px" + 
+    "> <tr>" +
+        "<th width = 100px>Method</th>" +
+        "<th width = 100px>Endpoint</th>" +
+        "<th width = 100px>Request body (JSON)</th>"+
+        "<th width = 100px>Response(JSON)</th>"+
+        "<th width = 100px>Error</th>"+
+    "</tr>"+
+    "<tr>" +
+            "<th>GET</th>"+
+            "<th>api/person/all</th>"+
+            "<th></th>" +
+            "<th>[user,user,...] (array of users)</th>" +
+            "<th></th>" +
+        "</tr>" +
+        "<tr>" +
+            "<th>GET</th>" +
+            "<th>api/person/{id}</th>"+
+            "<th></th>"+
+            "<th>user (with the requested id)</th>"+
+            "<th>(e1)</th>" +
+        "</tr>" +
+    "</table>";
+});
 
 
 
